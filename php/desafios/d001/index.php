d35b0461e81d94f7f035fc7d42c2354ccf52b8a1
Kelvin Soares <146841363+khelvin-soares@users.noreply.github.com>
2023-10-28 10:20:04 -0300

    desafio 001 concluído
    Concluí o primeiro desafio que foi passado no curso!

---
+++ php/desafios/d001/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Antecessor e Sucessor</title>
+</head>
+<body>
+    <main>
+        <?php
+            $num = $_POST['num'];
+            echo("<h1>Resultado final</h1>");
+            echo("<p>O número escolhido foi " . $num . ".</p>");
+            echo("<p>O seu antecessor é " . $num-1 . ".</p>");
+            echo("<p>O seu Sucessor é " . $num+1 . ".</p>");
+        ?>
+        <a href="javascript:history.go(-1)"><button>&lsaquo; Voltar</button></a>
+    </main>
+</body>
+</html>
