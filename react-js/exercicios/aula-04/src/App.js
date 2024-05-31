69a1062423f16d7cb2073542009269d51146b602
Kelvin Soares <146841363+khelvin-soares@users.noreply.github.com>
2024-05-31 11:02:18 -0300

    Pasta da aula 04 criada
    Os arquivos de estudo da aula 04 já foram adicionados ao repositório!

---
+++ react-js/exercicios/aula-04/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Frase from './components/scripts/script.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Frase />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-js/exercicios/aula-04/src/components/scripts/script.jsx
@@ -0,0 +1,10 @@
+import '../styles/styles.css';
+
+export default function Frase() {
+    return (
+        <div className="div_Container">
+            <h1>CSS Modules</h1>
+            <p>Estou aprendendo CSS Modules!</p>
+        </div>
+    )
+}
