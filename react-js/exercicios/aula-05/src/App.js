7dd521bcd678a73733ee5c63e17bf7ace7f7959e
Kelvin Soares <146841363+khelvin-soares@users.noreply.github.com>
2024-06-16 14:39:08 -0300

    Aula 05 adicionada!
    Adicionei os arquivos da aula 05.

---
+++ react-js/exercicios/aula-05/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import List from './components/List.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-js/exercicios/aula-05/src/components/List.jsx
@@ -0,0 +1,13 @@
+function List() {
+    return (
+        <>
+            <h1>Estudando Fragments</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </>
+    );
+}
+
+export default List
